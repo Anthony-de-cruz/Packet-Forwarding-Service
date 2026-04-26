ffe4e2856d5fd3fb23f0cf03782a948454d29754
Anthony de Cruz <anthonydecruz@proton.me>
2026-04-26 13:49:34 +0100

    Fixed scripts to include the forwarding of the UE pool subnet.

--- scripts/init-open5gs-environ.sh
+++ scripts/init-open5gs-environ.sh
@@ -5,7 +5,10 @@
 # Fail mode
 set -euo pipefail
 
+# Grab config values.
+source "$(dirname "${BASH_SOURCE[0]}")/config.sh"
+
 sudo sysctl -w net.ipv4.ip_forward=1
-sudo iptables -t nat -A POSTROUTING -s 10.45.0.0/16 ! -o ogstun -j MASQUERADE
+sudo iptables -t nat -A POSTROUTING -s "$UE_POOL_SUBNET" ! -o ogstun -j MASQUERADE
 sudo iptables -I FORWARD 1 -i ogstun -j ACCEPT
 sudo iptables -I FORWARD 1 -o ogstun -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
