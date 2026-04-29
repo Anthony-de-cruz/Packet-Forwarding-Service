3afe7fb393228727487f396b0a2c7d129a88aa64
Anthony de Cruz <anthonydecruz@proton.me>
2026-04-29 18:55:12 +0100

    Changed script config names.

--- scripts/router-init-env.sh
+++ scripts/router-init-env.sh
@@ -6,10 +6,10 @@
 # set -euo pipefail
 
 # Grab config values.
-source "$(dirname "${BASH_SOURCE[0]}")/config.sh"
+# source "$(dirname "${BASH_SOURCE[0]}")/config.sh"
 
 # Run reset script with absolute path.
-"$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)/reset.sh"
+"$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)/router-reset-env.sh"
 
 echo "[+] Enabling IPv4 forwarding..."
 sudo sysctl -w net.ipv4.ip_forward=1
@@ -17,17 +17,17 @@ sudo modprobe br_netfilter
 sudo sysctl -w net.bridge.bridge-nf-call-iptables=1
 
 echo "[+] Configuring firewall mark policy routes..."
-sudo ip rule add fwmark $FW_MARK_1 table $ROUTE_TABLE_1
-sudo ip route add default via $NEXT_HOP_1 table $ROUTE_TABLE_1
+sudo ip rule add fwmark $HOST_FW_MARK_1 table $HOST_ROUTE_TABLE_1
+sudo ip route add default via $NODE_HOP_1 table $HOST_ROUTE_TABLE_1
 
-sudo ip rule add fwmark $FW_MARK_2 table $ROUTE_TABLE_2
-sudo ip route add default via $NEXT_HOP_2 table $ROUTE_TABLE_2
+sudo ip rule add fwmark $HOST_FW_MARK_2 table $HOST_ROUTE_TABLE_2
+sudo ip route add default via $NODE_HOP_2 table $HOST_ROUTE_TABLE_2
 
-sudo ip rule add fwmark $FW_MARK_3 table $ROUTE_TABLE_3
-sudo ip route add default via $NEXT_HOP_3 table $ROUTE_TABLE_3
+sudo ip rule add fwmark $HOST_FW_MARK_3 table $HOST_ROUTE_TABLE_3
+sudo ip route add default via $NODE_HOP_3 table $HOST_ROUTE_TABLE_3
 
-sudo ip rule add fwmark $FW_MARK_4 table $ROUTE_TABLE_4
-sudo ip route add default via $NEXT_HOP_4 table $ROUTE_TABLE_4
+sudo ip rule add fwmark $HOST_FW_MARK_4 table $HOST_ROUTE_TABLE_4
+sudo ip route add default via $NODE_HOP_4 table $HOST_ROUTE_TABLE_4
 
 echo "[+] Redirecting bridge ingress traffic to NFQueue $NFQUEUE_NUM before routing..."
 # sudo iptables -t mangle -A OUTPUT \
@@ -36,13 +36,13 @@ echo "[+] Redirecting bridge ingress traffic to NFQueue $NFQUEUE_NUM before rout
     # -p icmp --icmp-type echo-request \
     # -j LOG --log-prefix "MARKED_UDP: " \
 sudo iptables -t mangle -A PREROUTING \
-    -i "$BRIDGE_NAME" \
-    -j NFQUEUE --queue-num "$NFQUEUE_NUM" --queue-bypass
+    -i "$VM_BRIDGE_NAME" \
+    -j NFQUEUE --queue-num "$HOST_NFQUEUE_NUM" --queue-bypass
 
 echo "[!] Router setup complete!"
 sudo iptables -t mangle -nvL PREROUTING --line-numbers
 sudo ip rule list
-sudo ip route show table $ROUTE_TABLE_1
-sudo ip route show table $ROUTE_TABLE_2
-sudo ip route show table $ROUTE_TABLE_3
-sudo ip route show table $ROUTE_TABLE_4
+sudo ip route show table $HOST_ROUTE_TABLE_1
+sudo ip route show table $HOST_ROUTE_TABLE_2
+sudo ip route show table $HOST_ROUTE_TABLE_3
+sudo ip route show table $HOST_ROUTE_TABLE_4
--- scripts/router-reset-env.sh
+++ scripts/router-reset-env.sh
@@ -10,25 +10,25 @@ source "$(dirname "${BASH_SOURCE[0]}")/config.sh" -euo pipefail
 
 # Flush project routing policy.
 echo "[-] Removing policy routing rules..."
-while sudo ip rule del fwmark "$FW_MARK_1" table "$ROUTE_TABLE_1" 2>/dev/null; do :; done
-while sudo ip rule del fwmark "$FW_MARK_2" table "$ROUTE_TABLE_2" 2>/dev/null; do :; done
-while sudo ip rule del fwmark "$FW_MARK_3" table "$ROUTE_TABLE_3" 2>/dev/null; do :; done
-while sudo ip rule del fwmark "$FW_MARK_4" table "$ROUTE_TABLE_4" 2>/dev/null; do :; done
+while sudo ip rule del fwmark "$HOST_FW_MARK_1" table "$HOST_ROUTE_TABLE_1" 2>/dev/null; do :; done
+while sudo ip rule del fwmark "$HOST_FW_MARK_2" table "$HOST_ROUTE_TABLE_2" 2>/dev/null; do :; done
+while sudo ip rule del fwmark "$HOST_FW_MARK_3" table "$HOST_ROUTE_TABLE_3" 2>/dev/null; do :; done
+while sudo ip rule del fwmark "$HOST_FW_MARK_4" table "$HOST_ROUTE_TABLE_4" 2>/dev/null; do :; done
 
-sudo ip route flush table $ROUTE_TABLE_1 2>/dev/null || true
-sudo ip route flush table $ROUTE_TABLE_2 2>/dev/null || true
-sudo ip route flush table $ROUTE_TABLE_3 2>/dev/null || true
-sudo ip route flush table $ROUTE_TABLE_4 2>/dev/null || true
+sudo ip route flush table $HOST_ROUTE_TABLE_1 2>/dev/null || true
+sudo ip route flush table $HOST_ROUTE_TABLE_2 2>/dev/null || true
+sudo ip route flush table $HOST_ROUTE_TABLE_3 2>/dev/null || true
+sudo ip route flush table $HOST_ROUTE_TABLE_4 2>/dev/null || true
 
 # Remove project packet handling rules.
 echo "[-] Removing project iptables rules..."
-while sudo iptables -t mangle -D OUTPUT -j NFQUEUE --queue-num "$NFQUEUE_NUM" --queue-bypass 2>/dev/null; do :; done
-while sudo iptables -t mangle -D OUTPUT -p udp --dport 9000 -j NFQUEUE --queue-num "$NFQUEUE_NUM" --queue-bypass 2>/dev/null; do :; done
-while sudo iptables -t mangle -D PREROUTING -i "$BRIDGE_NAME" -j NFQUEUE --queue-num "$NFQUEUE_NUM" --queue-bypass 2>/dev/null; do :; done
-while sudo iptables -t mangle -D FORWARD -i "$BRIDGE_NAME" -o "$LAN_IFACE" -j NFQUEUE --queue-num "$NFQUEUE_NUM" --queue-bypass 2>/dev/null; do :; done
+while sudo iptables -t mangle -D OUTPUT -j NFQUEUE --queue-num "$HOST_NFQUEUE_NUM" --queue-bypass 2>/dev/null; do :; done
+while sudo iptables -t mangle -D OUTPUT -p udp --dport 9000 -j NFQUEUE --queue-num "$HOST_NFQUEUE_NUM" --queue-bypass 2>/dev/null; do :; done
+while sudo iptables -t mangle -D PREROUTING -i "$VM_BRIDGE_NAME" -j NFQUEUE --queue-num "$HOST_NFQUEUE_NUM" --queue-bypass 2>/dev/null; do :; done
+while sudo iptables -t mangle -D FORWARD -i "$VM_BRIDGE_NAME" -o "$HOST_LAN_IFACE" -j NFQUEUE --queue-num "$HOST_NFQUEUE_NUM" --queue-bypass 2>/dev/null; do :; done
 while sudo iptables -t nat -D POSTROUTING -j MASQUERADE 2>/dev/null; do :; done
-while sudo iptables -t nat -D POSTROUTING -s "$BRIDGE_SUBNET" -o "$LAN_IFACE" -j MASQUERADE 2>/dev/null; do :; done
-while sudo iptables -D FORWARD -i "$BRIDGE_NAME" -o "$LAN_IFACE" -s "$BRIDGE_SUBNET" -j ACCEPT 2>/dev/null; do :; done
-while sudo iptables -D FORWARD -i "$LAN_IFACE" -o "$BRIDGE_NAME" -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT 2>/dev/null; do :; done
+while sudo iptables -t nat -D POSTROUTING -s "$VM_BRIDGE_SUBNET" -o "$HOST_LAN_IFACE" -j MASQUERADE 2>/dev/null; do :; done
+while sudo iptables -D FORWARD -i "$VM_BRIDGE_NAME" -o "$HOST_LAN_IFACE" -s "$BRIDGE_SUBNET" -j ACCEPT 2>/dev/null; do :; done
+while sudo iptables -D FORWARD -i "$HOST_LAN_IFACE" -o "$VM_BRIDGE_NAME" -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT 2>/dev/null; do :; done
 
 echo "[!] Cleanup complete!"
