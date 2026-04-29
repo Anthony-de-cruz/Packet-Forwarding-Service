f32c62c730444af2301e56600d878f99e2593cfd
Anthony de Cruz <anthonydecruz@proton.me>
2026-04-29 11:51:51 +0100

    Fixed up ueransim startup script.

--- scripts/vm-ueransim-init-env.sh
+++ scripts/vm-ueransim-init-env.sh
@@ -1,2 +1,10 @@
-./nr-gnb -c ../config/open5gs-gnb.yaml
-sudo ./nr-ue -c ../config/open5gs-ue.yaml
+#!/usr/bin/env bash
+
+BUILD_PATH="../../UERANSIM/build"
+
+echo "[+] Starting UERANSIM gNB..."
+"$BUILD_PATH"/nr-gnb -c ./vm-ueransim-open5gs-gnb.yaml &
+
+echo "[+] Starting UERANSIM UE..."
+sudo "$BUILD_PATH"/nr-ue -c ./vm-ueransim-open5gs-ue-0.yaml &
+#sudo "$BUILD_PATH"/nr-ue -c ./vm-ueransim-open5gs-ue.yaml &
