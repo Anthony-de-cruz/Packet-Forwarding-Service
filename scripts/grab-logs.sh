3a07c8c74f41eae96e7de4568b49e17fd30dcf30
Anthony de Cruz <anthonydecruz@proton.me>
2026-05-23 17:08:25 +0100

    Made new script to grab remote logs.

---
+++ scripts/grab-logs.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+scp user@192.168.0.52:~/Packet-Forwarding-Service/out/udp-echo-server.csv out/rpi-2-udp-echo-server.csv
+scp user@10.0.0.3:~/Packet-Forwarding-Service/out/udp-echo-client.csv out/vm-udp-echo-client.csv
