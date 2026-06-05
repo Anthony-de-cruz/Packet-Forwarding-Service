dcdf8ec56e3d18a00da6b5b723c1a32d93d08c7c
Anthony de Cruz <anthonydecruz@proton.me>
2026-06-05 15:47:48 +0100

    Pinned build dev flake to nixos-26.05 rather than unstable.

--- flake.nix
+++ flake.nix
@@ -2,8 +2,11 @@
   description = "nix flake for installing and linking with libnetfilter_queue and openssl.";
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
   outputs =
     {
@@ -23,13 +26,13 @@
       {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
-            rust-bin.stable.latest.default
-            uv
-            libnetfilter_queue
-            openssl
-            pkg-config
-            quickemu
-            spice
+            rust-bin.stable.latest.default # Service buildtime dep.
+            libnetfilter_queue # Service runtime dep.
+            openssl # ONNX runtime dep.
+            uv # Python dependency manager.
+            quickemu # QEMU setup.
+            spice # QEMU GUI.
+            pkg-config # Link shared objects (nix).
           ];
           shellHook = ''
             export PKG_CONFIG_PATH="${pkgs.libnetfilter_queue}/lib/pkgconfig:${pkgs.openssl}/lib/pkgconfig"
